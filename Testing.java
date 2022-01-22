cec1f84977d40d8e8b2588201f75ffff9f9fe706
Neha <nehabrahmbhatt5@yahoo.co.uk>
2022-01-22 12:32:20 +0000

    my first java files

---
+++ Testing.java
@@ -0,0 +1,21 @@
+package Learning;
+public class Testing{
+
+    public static void main(String[] args) {
+        String st1 ="Well come 2022 today is my birthday";
+        String st2 =" 2022";
+        String st3 ="Today is";
+        String st4 ="my birthday";
+        System.out.print(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(st4);
+
+
+
+    }
+
+
+
+    }
+
---
+++ Testing2.java
@@ -0,0 +1,15 @@
+package Learning;
+
+public class Testing2 {
+    public static void main(String[] args) {
+        int time = 20;
+        if (time > 20) {
+            System.out.println("good morning");
+        }else if ( time > 20){
+            System.out.println("good afternoon");
+
+        }else{
+            System.out.println("good evening");
+
+    }
+}}
