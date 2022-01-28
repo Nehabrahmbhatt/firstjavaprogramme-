3948aae5604ccc594bd0154ee21ea74ad3f519e3
neha <nehabrahmbhatt5@yahoo.co.uk>
2022-01-28 10:49:01 +0000

    this is my second commit

--- Testing2.java
+++ Testing2.java
@@ -2,7 +2,7 @@ package Learning;
 
 public class Testing2 {
     public static void main(String[] args) {
-        int time = 20;
+        int time = 30;
         if (time > 20) {
             System.out.println("today good morning");
         }else if ( time > 20){
